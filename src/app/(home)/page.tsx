bfca904577d8e2c199451adb651c1f5e188860f4
Gabriel Souza <gabrielmacielsouza537@gmail.com>
2023-12-10 16:32:41 -0300

    feat: add second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
         src="/banner-home-01.svg"
         height={0}
         width={0}
-        className="h-auto w-full px-5"
+        className="mt-2 h-auto w-full px-5"
         sizes="100vw"
         alt="Até 55% de desconto esse mês!"
       />
@@ -26,8 +26,18 @@ export default async function Home() {
       </div>
 
       <div className="mt-8">
+        <p className="mb-3 pl-5 font-bold uppercase">Ofertas</p>
         <ProductList products={deals} />
       </div>
+
+      <Image
+        src="/banner-home-02.svg"
+        height={0}
+        width={0}
+        className="mt-2 h-auto w-full px-5"
+        sizes="100vw"
+        alt="Até 55% de desconto em mouses!"
+      />
     </div>
   );
 }
