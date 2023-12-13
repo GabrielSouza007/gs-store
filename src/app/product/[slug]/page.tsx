6156b2495870b9d1632f19feebc53dc297300573
Gabriel Souza <gabrielmacielsouza537@gmail.com>
2023-12-13 17:10:10 -0300

    feat: ensure recommended products are shown on products details page

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import ProductList from "@/components/ui/product-list";
 import { computeProductTotalPrices } from "@/helpers/products";
 import { db } from "@/lib/prisma";
 import ProductImages from "./components/products-image";
@@ -16,14 +17,28 @@ const ProductsDetailsPage = async ({
     where: {
       slug: slug,
     },
+    include: {
+      category: {
+        include: {
+          products: {
+            where: {
+              slug: {
+                not: slug,
+              },
+            },
+          },
+        },
+      },
+    },
   });
 
   if (!products) return null;
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-8 pb-8">
       <ProductImages imageUrls={products.imageUrls} name={products.name} />
       <ProductsInfo product={computeProductTotalPrices(products)} />
+      <ProductList products={products.category.products} />
     </div>
   );
 };
---
+++ src/components/ui/product-list.tsx
@@ -0,0 +1,24 @@
+import ProductItem from "@/components/ui/product-item";
+import { computeProductTotalPrices } from "@/helpers/products";
+import { Product } from "@prisma/client";
+
+interface ProductListProps {
+  products: Product[];
+}
+
+const ProductList = ({ products }: ProductListProps) => {
+  return (
+    <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
+      {products.map((product) => (
+        <div className="w-[156px] max-w-[170px]">
+          <ProductItem
+            key={product.id}
+            product={computeProductTotalPrices(product)}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
