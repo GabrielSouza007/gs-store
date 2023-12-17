f92730e63328c696c45b77fc7ba85aa2eb00750b
Gabriel Souza <gabrielmacielsouza537@gmail.com>
2023-12-17 10:13:05 -0300

    feat: show total, subtotal and total discount on cart

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -4,11 +4,12 @@ import { ShoppingCartIcon } from "lucide-react";
 import { useContext } from "react";
 import { Badge } from "./badge";
 import CartItem from "./cart-items";
+import { Separator } from "./separator";
 
 interface CartProps {}
 
 const Cart = () => {
-  const { products } = useContext(CartContext);
+  const { products, subTotal, total, totalDiscount } = useContext(CartContext);
   return (
     <div className="flex flex-col gap-8">
       <Badge
@@ -20,12 +21,47 @@ const Cart = () => {
       </Badge>
 
       <div className="flex flex-col gap-5">
-        {products.map((product) => (
-          <CartItem
-            key={product.id}
-            products={computeProductTotalPrices(product as any) as any}
-          />
-        ))}
+        {products.length > 0 ? (
+          products.map((product) => (
+            <CartItem
+              key={product.id}
+              products={computeProductTotalPrices(product as any) as any}
+            />
+          ))
+        ) : (
+          <p className="text-center font-semibold">
+            Carrinho vazio. Vamos fazer compras ?
+          </p>
+        )}
+      </div>
+      <div className="flex flex-col gap-3">
+        <Separator />
+
+        <div className="flex items-center justify-between text-xs">
+          <p>Subtotal</p>
+          <p>R$ {subTotal.toFixed(2)}</p>
+        </div>
+
+        <Separator />
+
+        <div className="flex items-center justify-between text-xs">
+          <p>Entrega</p>
+          <p>Grátis</p>
+        </div>
+
+        <Separator />
+
+        <div className="flex flex-wrap items-center justify-between text-xs">
+          <p>Descontos</p>
+          <p>- R$ {totalDiscount.toFixed(2)}</p>
+        </div>
+
+        <Separator />
+
+        <div className="flex items-center justify-between text-xs font-bold">
+          <p>Total</p>
+          <p>R$ {total.toFixed(2)}</p>
+        </div>
       </div>
     </div>
   );
