d14a8199dfafd9396a5d4c3c26410eb6693de4d8
Gabriel Souza <gabrielmacielsouza537@gmail.com>
2023-12-23 00:12:19 -0300

    feat: add totals to order item

--- src/app/orders/components/order-item.tsx
+++ src/app/orders/components/order-item.tsx
@@ -5,8 +5,11 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { Card } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { computeProductTotalPrices } from "@/helpers/products";
 import { Prisma } from "@prisma/client";
 import { format } from "date-fns";
+import { useMemo } from "react";
 import OrderProductItem from "./order-product-item";
 
 interface OrderItemProps {
@@ -20,13 +23,34 @@ interface OrderItemProps {
 }
 
 const OrderItem = ({ order }: OrderItemProps) => {
+  const subtotal = useMemo(() => {
+    return order.orderProducts.reduce((acc, orderProduct) => {
+      return (
+        acc + Number(orderProduct.product.basePrice) * orderProduct.quantity
+      );
+    }, 0);
+  }, [order.orderProducts]);
+
+  const total = useMemo(() => {
+    return order.orderProducts.reduce((acc, product) => {
+      const productWithTotalPrice = computeProductTotalPrices(product.product);
+
+      return acc + productWithTotalPrice.totalPrice * product.quantity;
+    }, 0);
+  }, [order.orderProducts]);
+
+  const totalDiscounts = subtotal - total;
+
   return (
     <Card className="px-5">
       <Accordion type="single" className="w-full" collapsible>
         <AccordionItem value={order.id}>
           <AccordionTrigger>
             <div className="flex flex-col gap-1 text-left">
-              Pedido com {order.orderProducts.length} produto(s)
+              <p>Pedido com {order.orderProducts.length} produto(s)</p>
+              <span className="text-sm opacity-60">
+                Feito em {format(order.createdAt, "d/MM/y 'às' HH:mm")}
+              </span>
             </div>
           </AccordionTrigger>
 
@@ -50,12 +74,43 @@ const OrderItem = ({ order }: OrderItemProps) => {
                   <p className="opacity-60">Cartão</p>
                 </div>
               </div>
+
               {order.orderProducts.map((orderProduct) => (
                 <OrderProductItem
                   key={orderProduct.id}
                   orderProduct={orderProduct}
                 />
               ))}
+
+              <div className="flex w-full flex-col gap-1 text-xs">
+                <Separator />
+
+                <div className="flex w-full justify-between py-2">
+                  <p>Subtotal</p>
+                  <p>R${subtotal.toFixed(2)}</p>
+                </div>
+
+                <Separator />
+
+                <div className="flex items-center justify-between py-2 text-xs">
+                  <p>Entrega</p>
+                  <p>Grátis</p>
+                </div>
+
+                <Separator />
+
+                <div className="flex items-center justify-between py-2 text-xs">
+                  <p>Descontos</p>
+                  <p>-R${totalDiscounts.toFixed(2)}</p>
+                </div>
+
+                <Separator />
+
+                <div className="flex items-center justify-between py-2 text-sm font-bold">
+                  <p>Total</p>
+                  <p>R${total.toFixed(2)}</p>
+                </div>
+              </div>
             </div>
           </AccordionContent>
         </AccordionItem>
